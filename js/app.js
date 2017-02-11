b56c08c3694723e1a743a6b9a182865c057ec8e2
Seif <seif.dorani@gmail.com>
2017-02-11 17:14:04 -0500

    function works, message is logged

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope){
   $scope.helloWorld = function(){
-    console.log("this is the helloWorld controller function in the mainCtrl");
+    console.log("this is the helloWorld controller function in the mainCtrl, it is now working.");
   };
 });
