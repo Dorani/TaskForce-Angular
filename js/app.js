437d69206c67812a6249976d7ee5ec5760b52ca5
Seif <seif.dorani@gmail.com>
2017-02-15 11:12:55 -0500

    created array of objects with a name key and value, this is a quick test to see if var are accesible

--- js/app.js
+++ js/app.js
@@ -3,4 +3,13 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function(){
     console.log("this is the helloWorld controller function in the mainCtrl, it is now working.");
   };
+  //creating fake todos array for testing purposes
+  $scope.todos = [
+    {"name": "clean house"},
+    {"name": "water plant"},
+    {"name": "dishes"},
+    {"name": "vaccum"},
+    {"name": "dog walking"},
+    {"name": "code"},
+  ]
 });
