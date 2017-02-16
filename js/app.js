2f31db96ede84eb5c6f50d14fa60400a30c86c40
Seif <seif.dorani@gmail.com>
2017-02-16 11:34:04 -0500

    notes in js file on service defining, parameters used, this keyword in play

--- js/app.js
+++ js/app.js
@@ -12,4 +12,12 @@ angular.module("todoListApp", [])
     {"name": "dog walking"},
     {"name": "code"},
   ]
-});
+})
+.service('');//method chaining, chain methods 1 after the other
+//define new service with service methods
+//1st parameter is the name of the service
+//2nd parameter is an annom callback function where we store our services methods
+//we will be using this keyword
+//this is special in js, context specific keyword that refers to specific things based on how its used
+//this refers to the service itself, when i attach the method helloConsole, it will be availbale of a method
+//console log.
