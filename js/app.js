35fc2b60a99ea2fbd11c95ddcf98afc6eef123b3
Seif <seif.dorani@gmail.com>
2017-02-16 12:24:51 -0500

    using this method inside our controller

--- js/app.js
+++ js/app.js
@@ -4,14 +4,19 @@ angular.module("todoListApp", [])
   $scope.learningNgChange = function(){
     console.log("an input has been changed");
   };
+  $scope.helloWorld = dataService.helloWorld;
+  $scope.todos = dataService.getTodos();
   //creating fake todos array for testing purposes
 
 })
-.service('dataService', function(){
+.service('dataService', function($http){//get request using http provider, in the services arguments
   this.helloConsole = function(){
     console.log('this is hello console service!');
-  }
-  this.getTodos =
+  };
+  this.getTodos = $http.get('mock/todos.json')//path on server, then method to request, execute code after res is received from sercer
+   .then(function(response){ //annom function callback with a paremeter of reponse received from server
+     return response.data;//todos
+   })
 });
 
 
@@ -23,3 +28,5 @@ angular.module("todoListApp", [])
 //this is special in js, context specific keyword that refers to specific things based on how its used
 //this refers to the service itself, when i attach the method helloConsole, it will be availbale of a method
 //console log.
+
+//http provider, dependency injection at work again!
