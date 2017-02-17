668fa3be077c70388bbf6d888bbb235262556b23
Seif <seif.dorani@gmail.com>
2017-02-17 13:43:40 -0500

    removing element, added to delete function, when true

--- js/app.js
+++ js/app.js
@@ -12,9 +12,9 @@ angular.module("todoListApp", [])
   $scope.todos = response.data;
   });
 
-  $scope.deleteTodo = function(todo) {
+  $scope.deleteTodo = function(todo, $index) {
     dataService.deleteTodo(todo);
-
+    $scope.todos.splice($index, 1);
   };
   $scope.saveTodo = function(todo) {
     dataService.saveTodo(todo);
