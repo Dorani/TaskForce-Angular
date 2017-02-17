cfefec935c952f46a65f48c14cd778835dadff2d
Seif <seif.dorani@gmail.com>
2017-02-17 10:39:25 -0500

    fire delete todo method on the service

--- js/app.js
+++ js/app.js
@@ -18,16 +18,16 @@ angular.module("todoListApp", [])
   this.helloWorld = function() {
     console.log("This is the data service's method!!");
   };
-
   this.getTodos = function(callback){
     $http.get('mock/todos.json')
     .then(callback);
   };
   this.deleteTodos = function(todo){
     console.log('the' + todo.name + 'todo has been deleted')
-
   };
-  
+  this.saveTodos = function(todo){
+    console.log('the' + todo.name + 'todo has been saved');
+  };
 });
 
 
