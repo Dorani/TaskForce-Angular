e38c34d98d2122ff728e88439f2367bf105c2b23
Seif <seif.dorani@gmail.com>
2017-02-20 13:05:37 -0500

    ability we have the ability to define controller inside directive

--- js/directives/todo.js
+++ js/directives/todo.js
@@ -2,5 +2,6 @@ angular.module('todoListApp')
 .directive('todos', function() {
   return {
     templateUrl: 'templates/todos.html'
+    controller: 'mainCtrl' //ability we have the ability to define controller inside directive
   }
 })
